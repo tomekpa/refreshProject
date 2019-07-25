6d480968ca62a3ef5281772b6a693f5837dbddeb
tomekpa <tomekpa@protonmail.com>
2019-07-25 17:24:32 +0200

    Java8 date API with zones, part2

--- src/test/java/javavanila/datetime/LocalDateGuideTest.java
+++ src/test/java/javavanila/datetime/LocalDateGuideTest.java
@@ -2,7 +2,9 @@ package javavanila.datetime;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Set;
@@ -28,7 +30,6 @@ public class LocalDateGuideTest {
         LocalDate localDate1 = LocalDate.of(2019, 12, 31);
         LocalDate localDate2 = LocalDate.of(2019, 12, 31);
         System.out.println("Same LocalDate can be compared with \"==\" operator: " + (localDate1 == localDate2));
-
     }
 
     @Test
@@ -37,8 +38,12 @@ public class LocalDateGuideTest {
         allZoneIds.stream().sorted().forEach(System.out::println);
     }
 
+    //https://stackoverflow.com/questions/40075780/java-best-practice-for-date-manipulation-storage-for-geographically-diverse-user
+
     @Test
     public void defaultJava8DateApiZones() {
+
+        //INFO Z means UTC = ZuluTime
 
         //ZoneId specifies a time zone identifier and provides rules for converting between an Instant and a LocalDateTime.
         //ZoneOffset specifies a time zone offset from Greenwich/UTC time.
@@ -56,11 +61,20 @@ public class LocalDateGuideTest {
         ZonedDateTime zonedDateTime1 = ZonedDateTime.of(localDateTime, zoneId1);
         System.out.println(zonedDateTime1);
         System.out.println(zonedDateTime1.getOffset());
+        System.out.println("== ZonedDateTime at " + zoneId1 + " stored as UTC");
+        ZonedDateTime zone1asUtc = zonedDateTime1.withZoneSameInstant(zoneId1);
+        System.out.println(zone1asUtc);
+        System.out.println(zone1asUtc.getOffset());
 
         System.out.println("== ZonedDateTime at " + zoneId2);
         ZonedDateTime zonedDateTime2 = ZonedDateTime.of(localDateTime, zoneId2);
         System.out.println(zonedDateTime2);
         System.out.println(zonedDateTime2.getOffset());
+
+        System.out.println("== ZonedDateTime at " + zoneId2 + " stored as UTC");
+        ZonedDateTime zone2asUtc = zonedDateTime1.withZoneSameInstant(zoneId2);
+        System.out.println(zone2asUtc);
+        System.out.println(zone2asUtc.getOffset());
 
         //INFO: We say that LocalDateTime we are talking about is in ZoneId thus we have ZonedDateTime
 
@@ -74,7 +88,7 @@ public class LocalDateGuideTest {
         System.out.println(zonedDateTime1AsUTCasZone1);
         System.out.println(zonedDateTime1AsUTCasZone1.getOffset());
 
-        System.out.println("== ZonedDateTime from UTC in zone2:" + zoneId1);
+        System.out.println("== ZonedDateTime from UTC in zone2:" + zoneId2);
         ZonedDateTime zonedDateTime1AsUTCasZone2 = zonedDateTime1AsUTC.withZoneSameInstant(zoneId2);
         System.out.println(zonedDateTime1AsUTCasZone2);
         System.out.println(zonedDateTime1AsUTCasZone2.getOffset());
@@ -100,17 +114,89 @@ public class LocalDateGuideTest {
         System.out.println("Days2:" + ChronoUnit.DAYS.between(localDateTime.toLocalDate(), inFifeDays.toLocalDate()));
     }
 
+    //TODO: sql.Date
     @Test
     public void defaultJava8Instant() {
         Instant instant = Instant.now();
         System.out.println(instant);
 
-        int nano = instant.getNano();
-        System.out.println(nano);
+        long epochSecond = instant.getEpochSecond();
+        System.out.println(epochSecond);
 
-        Date date = new Date(nano);
+        Date date = new Date(epochSecond * 1000);
         System.out.println(date);
         System.out.println(date.getTime());
 
+//        LocalDate localDate = LocalDate.from(instant); //Java9
+
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneOffset.UTC); //Java9
+        System.out.println(localDateTime.toLocalDate());
+
+        //ISO 8601 format contains Z at the end, meanint UTC+0
+
+        //If the time is in UTC, add a Z directly after the time without a space.
+        //Z is the zone designator for the zero UTC offset.
+        //Sometimes referred to as "Zulu time" However the ACP 121 standard that defines the list of military time zones makes
+        //no mention of UTC and derives the "Zulu time" from the Greenwich Mean Time[27]
+        System.out.println(Instant.now().toString());
+    }
+
+    @Test
+    public void defaultJava8UtilDateInstantConversions() {
+        //https://stackoverflow.com/questions/26142864/how-to-get-utc0-date-in-java-8
+        Instant instant = Instant.now();
+
+        Date dateFromInstant = Date.from(instant);
+        Instant InstantFromDate = dateFromInstant.toInstant();
+
+        System.out.println(instant);
+        System.out.println(InstantFromDate);
+        System.out.println(dateFromInstant);
+
+    }
+
+    @Test
+    public void defaultJava8OffsetDateTime() {
+        //Instant has limitations such as no formatting options for generating strings in alternate formats
+        Instant instant = Instant.now();
+        OffsetDateTime odt1 = instant.atOffset(ZoneOffset.UTC);
+        OffsetDateTime odt2 = OffsetDateTime.now(ZoneOffset.UTC);
+        OffsetDateTime odt3 = OffsetDateTime.now(ZoneId.of("Europe/Warsaw"));
+        System.out.println(DateTimeFormatter.RFC_1123_DATE_TIME.format(odt1));
+        System.out.println(DateTimeFormatter.RFC_1123_DATE_TIME.format(odt2));
+        System.out.println(DateTimeFormatter.RFC_1123_DATE_TIME.format(odt3));
+    }
+
+    @Test
+    public void defaultJava8HandlingDaylightSavingTimeDSTChange() throws ParseException {
+
+//        2018-03-25T00:55
+//        2018-03-25T00:55Z
+//        2018-03-25T01:55+01:00[Europe/Warsaw]
+//        2018-03-25T01:56+01:00[Europe/Warsaw]
+//        2018-03-25T01:57+01:00[Europe/Warsaw]
+//        2018-03-25T01:58+01:00[Europe/Warsaw]
+//        2018-03-25T01:59+01:00[Europe/Warsaw]
+//        2018-03-25T03:00+02:00[Europe/Warsaw]
+//        2018-03-25T03:01+02:00[Europe/Warsaw]
+//        2018-03-25T03:02+02:00[Europe/Warsaw]
+//        2018-03-25T03:03+02:00[Europe/Warsaw]
+//        2018-03-25T03:04+02:00[Europe/Warsaw]
+
+        LocalDateTime localDateTimeBeforeDST = LocalDateTime.of(2018, 3, 25, 0, 55);
+        System.out.println(localDateTimeBeforeDST.toString());
+
+
+        ZonedDateTime utcTime = ZonedDateTime.of(localDateTimeBeforeDST, ZoneOffset.UTC);
+        System.out.println(utcTime);
+
+        ZoneId zoneId = ZoneId.of("Europe/Warsaw");
+        ZonedDateTime warsawTime = utcTime.withZoneSameInstant(zoneId);
+        System.out.println(warsawTime);
+
+        for (int i = 1; i < 10; i++) {
+            ZonedDateTime newWarsawTime = warsawTime.plusMinutes(i);
+            System.out.println(newWarsawTime);
+        }
     }
 }
